fbc4e94296d66bf701aa3925321224f01d622fb3
Ham3798 <43927226+Ham3798@users.noreply.github.com>
2022-05-17 19:52:28 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-import 'package:easyandquick/screens/myapp.dart';
-import 'package:flutter/cupertino.dart';
 
-void main() {
-  runApp(const MyApp()); // 앱 실행. 앱의 메인 페이지 입력
-}
-=======
 import 'package:easyandquick/screens/login.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -13,6 +6,3 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(login());
 }
-
-
->>>>>>> yunsik
